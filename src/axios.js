d930029975a71ecd91325c3c77b2835ddfe09f27
chenghuan <508231435@qq.com>
2023-07-14 16:04:45 +0800

    拦截器格式修改

--- src/axios.js
+++ src/axios.js
@@ -9,34 +9,33 @@ const service = axios.create({
   });
 
 // 添加请求拦截器
-service.interceptors.request.use(function (config) {
-  // 在发送请求之前做些什么
-  const cookies=useCookies()
-  const token=cookies.get('admin-token')
-  if(token){
-    config.headers["token"]=token
+service.interceptors.request.use(
+  function (config) {
+    const cookies=useCookies()
+    const token=cookies.get('admin-token')
+    if(token){
+      config.headers["token"]=token
+    }
+    return config;
+  }, 
+  function (error) {
+    return Promise.reject(error);
   }
-  
-  return config;
-}, function (error) {
-  // 对请求错误做些什么
-  return Promise.reject(error);
-});
+);
 
 // 添加响应拦截器
-service.interceptors.response.use(function (response) {
-  // 2xx 范围内的状态码都会触发该函数。
-  // 对响应数据做点什么
-  return response.data.data;
-}, function (error) {
-  // 超出 2xx 范围的状态码都会触发该函数。
-  // 对响应错误做点什么
-  ElNotification({
-    message: error.response.data.msg || '请求失败',
-    type: 'error',
-    duration:3000
-})
-  return Promise.reject(error);
-});
+service.interceptors.response.use(
+  function (response) {
+    return response.data.data;
+  }, 
+  function (error) {
+    ElNotification({
+      message: error.response.data.msg || '请求失败',
+      type: 'error',
+      duration:3000
+    })
+    return Promise.reject(error);
+  }
+);
 
-  export default service
+export default service
