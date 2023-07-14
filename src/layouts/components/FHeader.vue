9b9a1854b7eeeca662970b6800a2e97ae1778ff7
chenghuan <508231435@qq.com>
2023-07-14 23:42:04 +0800

    刷新，全屏功能

--- src/layouts/components/FHeader.vue
+++ src/layouts/components/FHeader.vue
@@ -1,5 +1,45 @@
 <script setup>
 import { ArrowDown } from '@element-plus/icons-vue'
+import {showModal} from '~/composables/util'
+import { ElMessage } from 'element-plus'
+import {logout} from '~/api/manager'
+import { removeToken } from '~/composables/auth'
+import { useRouter } from 'vue-router'
+import {useStore} from 'vuex'
+import { useFullscreen } from '@vueuse/core'
+
+const { isFullscreen,toggle } = useFullscreen()
+const store=useStore()
+
+const router =useRouter()
+
+const handleLogout= async()=>{
+    const res=await logout()
+    if(res){
+        //移除token
+        removeToken()
+        //清除vuex
+        store.dispatch('logout')
+        //登录页面
+        router.push('/login')
+    }
+}
+
+const open=()=>{
+    showModal("是否退出登录").then(() => {
+        ElMessage({
+        type: 'success',
+        message: '退出登录',
+        })
+        handleLogout()
+    }).catch(()=>{})
+    
+}
+
+const handleRefresh=()=>{
+    location.reload()
+}
+
 </script>
 
 <template>
@@ -9,9 +49,13 @@ import { ArrowDown } from '@element-plus/icons-vue'
             盒马鲜生
         </span>
         <el-icon class="icon-btn"><fold/></el-icon>
-        <el-icon class="icon-btn"><Refresh/></el-icon>
+        <el-tooltip effect="dark" content="刷新" placement="bottom">
+            <el-icon  @click="handleRefresh" class="icon-btn"><Refresh/></el-icon>
+        </el-tooltip>
         <div class="ml-auto flex items-center">
-            <el-icon class="icon-btn"><full-screen/></el-icon>
+            <el-tooltip effect="dark" content="全屏" placement="bottom">
+                <el-icon class="icon-btn" @click="toggle"><full-screen v-if="!isFullscreen"/><aim v-else/></el-icon>
+            </el-tooltip>
             <el-dropdown class="dropdown">
                 <span class="flex items-center justify-center text-light-50 ">
                     <el-avatar class="mr-2" :size="25" :src="$store.state.user.avatar" />
@@ -23,8 +67,7 @@ import { ArrowDown } from '@element-plus/icons-vue'
                 <template #dropdown>
                 <el-dropdown-menu>
                     <el-dropdown-item>修改密码</el-dropdown-item>
-                    <el-dropdown-item>退出登录</el-dropdown-item>
-                    
+                    <el-dropdown-item  @click="open">退出登录</el-dropdown-item>
                 </el-dropdown-menu>
                 </template>
             </el-dropdown>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,42 +1,13 @@
+<script setup>
+
+</script>
+
 <template>
     <div>
-        后台首页{{ $store.state.user.username }}
-        <el-button @click="open">退出登录</el-button>
+        后台首页
     </div>
 </template>
 
-<script setup>
-import {showModal} from '~/composables/util'
-import { ElMessage } from 'element-plus'
-import {logout} from '~/api/manager'
-import { removeToken } from '~/composables/auth'
-import { useRouter } from 'vue-router'
-import {useStore} from 'vuex'
-const store=useStore()
+<style scoped>
 
-const router =useRouter()
-
-const handleLogout= async()=>{
-    const res=await logout()
-    if(res){
-        //移除token
-        removeToken()
-        //清除vuex
-        store.dispatch('logout')
-        //登录页面
-        router.push('/login')
-    }
-}
-
-const open=()=>{
-    showModal("是否退出登录").then(() => {
-        ElMessage({
-        type: 'success',
-        message: '退出登录',
-        })
-        handleLogout()
-    }).catch(()=>{})
-    
-}
-
-</script>
+</style>
