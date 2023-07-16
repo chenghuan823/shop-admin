921238aa99aa5d601996a0914b36f039362919e4
chenghuan <508231435@qq.com>
2023-07-16 17:12:31 +0800

    transition全局过渡动画

--- src/layouts/admin.vue
+++ src/layouts/admin.vue
@@ -15,10 +15,12 @@ import FTagList from './components/FTagList.vue'
             </el-aside>
             <el-main>
                 <FTagList></FTagList>
-                <router-view v-slot="{Component}">
-                    <keep-alive :max="10">
-                        <component :is="Component"></component>
-                    </keep-alive>
+                <router-view v-slot="{ Component }">
+                    <transition name="fade">
+                        <keep-alive :max="10">
+                            <component :is="Component"></component>
+                        </keep-alive>
+                    </transition>
                 </router-view>
             </el-main>
         </el-container>
@@ -26,7 +28,31 @@ import FTagList from './components/FTagList.vue'
 </template>
 
 <style scoped>
-.el-aside{
-    transition:all .2s;
+.el-aside {
+    transition: all .2s;
+}
+
+.fade-enter-from {
+    opacity: 0;
+}
+
+.fade-enter-to {
+    opacity: 1;
+}
+
+.fade-leave-from {
+    opacity: 1;
+}
+
+.fade-leave-to {
+    opacity: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+    transition: all .3s;
+}
+.fade-enter-active{
+    transition-delay: .3s;
 }
 </style>
