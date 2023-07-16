ddefc47ff7abb57700e61ce3574268d668d692da
chenghuan <508231435@qq.com>
2023-07-16 19:17:03 +0800

    骨架屏优化体验

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,36 +10,52 @@ const GetStatistics1=async ()=>{
 }
 GetStatistics1()
 
-// {
-// 				"title": "支付订单",
-// 				"value": 49,
-// 				"unit": "年",
-// 				"unitColor": "success",
-// 				"subTitle": "总支付订单",
-// 				"subValue": 49,
-// 				"subUnit": ""
-// 			},
 </script>
 
 <template>
     <div>
         <el-row :gutter="20">
+            <template v-if="panels.length===0">
+                <el-col :span="6" v-for="i in 4" :key="i">
+                    <el-skeleton style="width: 100%" animated loading>
+                        <template #template>
+                            <el-card shadow="hover" class="border-0">
+                                <template #header>
+                                    <div class="flex justify-between">
+                                        <el-skeleton-item variant="text" style="width: 50%;" />
+                                        <el-skeleton-item variant="text" style="width: 10%;" />
+                                    </div>
+                                </template>
+                                <!-- card body -->
+                                <el-skeleton-item variant="h3" style="width: 80%;" />
+                                <el-divider/>
+                                <div class="flex justify-between text-sm text-gray-500">
+                                    <el-skeleton-item variant="text" style="width: 50%;" />
+                                    <el-skeleton-item variant="text" style="width: 10%;" />
+                                </div>
+                            </el-card>
+                            
+                        </template>
+                    </el-skeleton>
+                </el-col>
+            </template>
+
             <el-col :span="6" :offset="0" v-for="(item,index) in panels" :key="index">
-            <el-card shadow="hover" class="border-0">
-                <template #header>
-                <div class="flex justify-between">
-                    <span class="text-sm">{{ item.title }}</span>
-                    <el-tag :type="unitColor">{{ item.unit }}</el-tag>
-                </div>
-                </template>
-                <!-- card body -->
-                <span class="text-3xl font-bold text-gray-500">{{ item.value }}</span>
-                <el-divider/>
-                <div class="flex justify-between text-sm text-gray-500">
-                    <span>{{ item.subTitle }}</span>
-                    <span>{{ item.subValue }}</span>
-                </div>
-            </el-card>
+                <el-card shadow="hover" class="border-0">
+                    <template #header>
+                    <div class="flex justify-between">
+                        <span class="text-sm">{{ item.title }}</span>
+                        <el-tag :type="item.unitColor">{{ item.unit }}</el-tag>
+                    </div>
+                    </template>
+                    <!-- card body -->
+                    <span class="text-3xl font-bold text-gray-500">{{ item.value }}</span>
+                    <el-divider/>
+                    <div class="flex justify-between text-sm text-gray-500">
+                        <span>{{ item.subTitle }}</span>
+                        <span>{{ item.subValue }}</span>
+                    </div>
+                </el-card>
             </el-col>
         </el-row>
         
